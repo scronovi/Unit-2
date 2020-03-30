5006e5fb13b4651f0b1954f6e86664030801f2be
scronovi <33103889+scronovi@users.noreply.github.com>
2020-03-30 10:14:06 +0200

    Updated app.py

--- app.py
+++ app.py
@@ -6,12 +6,19 @@ def cleanse_data(**kwargs):
     expTeam = []
     unexpTeam = []
 
-    for player in players.copy():
+    for player in players:
         for key, value in player.items():
             if key == 'experience' and value == 'YES':
                 expTeam.append(player)
             elif key == 'experience' and value == 'NO':
                 unexpTeam.append(player)
+
+    if len(expTeam) == len(unexpTeam):
+        #1/3 out of expteam and unexpteam to each team
+        #check how to get 1/3 of a list, or how to divide a list into 3
+        #then append to teams
+
+cleanse_data(players=constants.PLAYERS, teams=constants.TEAMS)
 
 if __name__ == '__main__':
     #main calls / code here
